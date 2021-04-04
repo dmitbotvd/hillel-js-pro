e0dba0a69f9aae1f00937625db75e81f2a7a0c82
scriptbot <dmitbotvd@gmail.com>
2021-04-04 13:56:57 +0300

    lesson add js

---
+++ lesson 2/js.js
@@ -0,0 +1,13 @@
+console.log(40 / "4"); //= 4 
+console.log(true + true); //= 2
+console.log("valera" + 123); //= valera123
+console.log(+"true"); //= NaN
+console.log(20 > '40'); //= false
+console.log(null >= 0); //= true
+console.log(null == undefined); //= true
+console.log(null === undefined); //= false
+console.log(true + 40 * 2); //= 81
+console.log('valera' >= 'valer'); // = true
+console.log(true || false ); //= true
+console.log(true && true && 'valera'); // = 'valera'
+console.log(false || 0 || ''); //= ''
