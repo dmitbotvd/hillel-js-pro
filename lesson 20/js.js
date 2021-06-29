28a50da8c563f02854647e60093dc3dce6b766eb
scriptbot <dmitbotvd@gmail.com>
2021-06-29 22:55:37 +0300

    add lesson 20

--- lesson 20/js.js
+++ lesson 20/js.js
@@ -1,5 +1,15 @@
 let table = document.querySelector('table');
-
 table.addEventListener('click', (e) => {
-  console.log(e.currentTarget.id);
+  let target = e.target;
+  if (target.classList.contains('green')) {
+    removeTd(target);
+  } else {
+    colorTd(target);
+  }
 });
+function colorTd(elem) {
+  elem.classList.add('green');
+}
+function removeTd(elem) {
+  elem.classList.remove('green');
+}
